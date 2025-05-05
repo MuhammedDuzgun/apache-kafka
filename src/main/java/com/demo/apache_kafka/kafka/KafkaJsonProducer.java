c08e0d00fa2b6c14ae46576b6b121669f4a7a0f0
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-05-06 01:24:24 +0300

    create KafkaJsonProducer class

---
+++ src/main/java/com/demo/apache_kafka/kafka/KafkaJsonProducer.java
@@ -0,0 +1,33 @@
+package com.demo.apache_kafka.kafka;
+
+import com.demo.apache_kafka.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaJsonProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaJsonProducer.class);
+
+    private final KafkaTemplate<String, User> kafkaTemplate;
+
+    public KafkaJsonProducer(KafkaTemplate<String, User> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(User user) {
+        //create message
+        Message<User> message = MessageBuilder
+                .withPayload(user)
+                .setHeader(KafkaHeaders.TOPIC, "app-topic")
+                .build();
+
+        kafkaTemplate.send(message);
+    }
+
+}
